ebba324559d52fead2f94dd0a0b68cb7b7999d5e
Kivert-AC <klivertalcocerc22@gmail.com>
2026-03-29 17:17:45 -0500

    Initial commit

---
+++ src/main/java/org/example/Cita.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Cita {
+
+    public String validar(String codigo, String nombre, String dni, String fecha) {
+        return "La cita ha sido registrada correctamente";
+    }
+}
---
+++ src/test/java/org/example/CitaTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CitaTest {
+    @Test
+    public void testCodigoValido() {
+        Cita cita = new Cita();
+        String resultado = cita.validar("C123", "Carlos", "12345678", "2026-12-01");
+        assertEquals("La cita ha sido registrada correctamente", resultado);
+    }
+}
