860358673f26328f903ad11635a3e7484c33601e
Kivert-AC <klivertalcocerc22@gmail.com>
2026-03-29 18:17:00 -0500

    Mis Tests y Clses

--- src/main/java/org/example/Cita.java
+++ src/main/java/org/example/Cita.java
@@ -1,8 +1,32 @@
 package org.example;
 
+import java.time.LocalDate;
+
 public class Cita {
 
     public String validar(String codigo, String nombre, String dni, String fecha) {
+
+        if (codigo.isEmpty() || nombre.isEmpty() || dni.isEmpty() || fecha.isEmpty()) {
+            return "Debe ingresar todos los datos requeridos";
+        }
+
+        if (!codigo.matches("C\\d{3}")) {
+            return "Ingrese un código de cita válido";
+        }
+
+        if (!nombre.matches("[a-zA-Z]{5,}")) {
+            return "El nombre del paciente debe tener al menos cinco caracteres alfabéticos";
+        }
+
+        if (!dni.matches("\\d{8}")) {
+            return "Ingrese un número de documento válido";
+        }
+
+        LocalDate fechaCita = LocalDate.parse(fecha);
+        if (!fechaCita.isAfter(LocalDate.now())) {
+            return "La fecha de la cita debe ser posterior a la fecha actual";
+        }
+
         return "La cita ha sido registrada correctamente";
     }
 }
--- src/test/java/org/example/CitaTest.java
+++ src/test/java/org/example/CitaTest.java
@@ -19,4 +19,36 @@ public class CitaTest {
         String resultado = cita.validar("1234", "Carlos", "12345678", "2026-12-01");
         assertEquals("Ingrese un código de cita válido", resultado);
     }
+
+    //Mi Tercer Test
+    @Test
+    public void testNombreInvalido() {
+        Cita cita = new Cita();
+        String resultado = cita.validar("C123", "Ana", "12345678", "2026-12-01");
+        assertEquals("El nombre del paciente debe tener al menos cinco caracteres alfabéticos", resultado);
+    }
+
+    //Mi cuarto Test
+    @Test
+    public void testDniInvalido() {
+        Cita cita = new Cita();
+        String resultado = cita.validar("C123", "Carlos", "1234", "2026-12-01");
+        assertEquals("Ingrese un número de documento válido", resultado);
+    }
+
+    //MI Quinto Test
+    @Test
+    public void testFechaInvalida() {
+        Cita cita = new Cita();
+        String resultado = cita.validar("C123", "Carlos", "12345678", "2020-01-01");
+        assertEquals("La fecha de la cita debe ser posterior a la fecha actual", resultado);
+    }
+
+    //Mi sexto Test
+    @Test
+    public void testCamposVacios() {
+        Cita cita = new Cita();
+        String resultado = cita.validar("", "", "", "");
+        assertEquals("Debe ingresar todos los datos requeridos", resultado);
+    }
 }
