8b6c5aea837fd2444356c81777afa4d03fd7ac15
Kivert-AC <klivertalcocerc22@gmail.com>
2026-03-29 17:27:03 -0500

    Mi Segundo Test and segundo push

--- src/test/java/org/example/CitaTest.java
+++ src/test/java/org/example/CitaTest.java
@@ -11,4 +11,12 @@ public class CitaTest {
         String resultado = cita.validar("C123", "Carlos", "12345678", "2026-12-01");
         assertEquals("La cita ha sido registrada correctamente", resultado);
     }
+
+    //Mi segundo Test and segundo push
+    @Test
+    public void testCodigoInvalido() {
+        Cita cita = new Cita();
+        String resultado = cita.validar("1234", "Carlos", "12345678", "2026-12-01");
+        assertEquals("Ingrese un código de cita válido", resultado);
+    }
 }
